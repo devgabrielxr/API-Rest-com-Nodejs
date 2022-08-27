96036ec1f47c4ad79a529d1382198d11cd5a6b31
Gabriel Xavier Rodrigues <gabrielxavier2.0@hotmail.com>
2022-08-27 16:42:26 -0300

    Adicionando padrão MVC

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js"
 import livros from "./models/Livro.js"
+import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro de Conexão'));
 db.once("open", ()=>{
@@ -10,22 +11,12 @@ const app = express();
 
 app.use(express.json());
 
-
+routes(app);
 
 //const livros = [
 //   {id: 1, "titulo": "Harry Potter"},
 //    {id: 2, "titulo": "Anne frank"}
 //]
-
-app.get('/', (req,res) => {
-    res.status(200).send('Curso de Node');
-})
-
-app.get('/livros',(req,res)=>{
-    livros.find((err, livros) => {
-        res.status(200).json(livros)
-    })   
-})
 
 app.get('/livros/:id', (req,res)=>{
     let index = buscaLivro(req.params.id);
